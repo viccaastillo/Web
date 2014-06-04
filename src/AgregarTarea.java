ec8c3132f498b84fb6aa25278334cecec7123bd3
victor Castillo <v.castillo.mail@gmail.com>
2014-06-04 14:07:02 -0500

    Agergar la tarea

--- src/AgregarTarea.java
+++ src/AgregarTarea.java
@@ -2,6 +2,9 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -31,7 +34,7 @@ public class AgregarTarea extends HttpServlet {
 				+"<tr>"
 				+"<td>Title:</td>"
 				+"<td>"
-				+"<input type=\"Text\" name\"title\">"
+				+"<input type=\"Text\" name=\"titulo\">"
 				+"</td>"
 				+"</tr>"
 				+"<td>State:</td>"
@@ -42,45 +45,69 @@ public class AgregarTarea extends HttpServlet {
 				+"<option value=\"IN PROGRESS\"> In progress</option>"
 				+"<option value=\"DONE\"> Done </option>"
 				+"</select>"
-				+"<input type=\"Text\" name\"State\">"
+				+"<input type=\"Text\" name=\"State\">"
 				+"</td>"
 				+"</tr>"
 				+"<tr>"
 				+"<td>Descripcion:</td>"
 				+"<td>"
-				+"<input type=\"Text\" name\"Description\">"
+				+"<input type=\"Text\" name=\"Description\">"
 				+"</td>"
 				+"</tr>"
 				+"<tr>"
-				+"<td>FechaEntrega:</td>"
+				+"<td>dueDate:</td>"
 				+"<td>"
-				+"<input type=\"Text\" name\"DueDate\">"
+			
+				+"<td><input type=\"text\" name=\"due_date\" value=\""+ new Date() + "\"/></td>"
 				+"</td>"
 				+"</tr>"
 				+"<tr>"
-				+"<imput type=\"submit\" value=\"Add\"/>"
-				+"</td>"
+			
+				+"<td colspan=\"2\"><input type=\"submit\" value=\"Add\" /></td>"
 				+"</tr>"
 				+"</table>"
-				+"</form>"	
-				+"</body>"
-				+"</html>"
-				
+				+"</form>"
+				+ "</body>"
+				+"</html>"				
 				);
 		
 		
 	
 	}
+	Task Tarea = new Task();
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
-		String title=request.getParameter("title");
-				
-				response.setContentType("text/html");
-		PrintWriter writer= response.getWriter();
-		writer.print("<html>"
-				+"<body>"
-				+"Title: "+title
-				+"</body>"
-				+"</html>");
+		
+		Tarea.setTitle(request.getParameter("title"));
+		Tarea.setDescription(request.getParameter("description"));
+	Tarea.setEntrega(request.getParameter("due_date"));
+	
+	Tarea.setEstado((request.getParameter("State")));
+	 
+		/*try {
+			Tarea.setState(State.valueOf(request.getParameter("State")));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		*/
+	
+
+		if(confirmacion()==true){
+
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		writer.print("<html><body>" +"La Tarea se Agrego con exito"+ "</body></html>"+"<br>");
+		writer.print("<html><body>" +"Con el titulo: "+ request.getParameter("titulo")+"<br>");
+		writer.print("<html><body>" +"Fecha de ngreso: "+ new Date()+"<br>");
+		writer.print("<html><body>" +"Fecha de Entrega: "+ request.getParameter("due_date")+"<br>" );
+	
+		}
+	}
+	
+	public boolean confirmacion (){
+		Programa.dashboard.add(Tarea);
+		return true;
+		
 	}
 	
 }
